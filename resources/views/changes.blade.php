664862f9f15d7480e97c9fed2bcdcca48647914d
Yehor Bobrov <yehorbobrov8@gmail.com>
2017-03-09 01:32:52 +0200

    add images

--- resources/views/changes.blade.php
+++ resources/views/changes.blade.php
@@ -3,97 +3,97 @@
 @section('content')
    <div class="container">
 	<div class="row">
-            <div class="col-md-8 col-md-offset-2">
 			
-			<div class="col-md-6">
+		<div class="col-md-4">
 			      <img class="img-responsive" src="images/ch13.jpg" alt="...">
 
 			</div>
 			
-			<div class="col-md-6">
+		<div class="col-md-4">
 			      <img class="img-responsive" src="images/ch1.jpg" alt="...">
 
 			</div>
 			
-			<div class="col-md-6">
+		<div class="col-md-4">
 			      <img class="img-responsive" src="images/ch2.jpg" alt="...">
 
 			</div>
 			
-			<div class="col-md-6">
+	</div>	
+	<div class="row">
+	
+		<div class="col-md-4">
 			      <img class="img-responsive" src="images/ch3.jpg" alt="...">
 
 			</div>
 			
-			<div class="col-md-6">
+		<div class="col-md-4">
 			      <img class="img-responsive" src="images/ch4.jpg" alt="...">
 
 			</div>
 			
-			<div class="col-md-6">
+		<div class="col-md-4">
 			      <img class="img-responsive" src="images/ch5.jpg" alt="...">
 
 			</div>
-			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-		
-		
-
-  <!-- Wrapper for slides -->
- 
-    <div class="item active">
-      <img class="img-responsive" src="images/ch13.jpg" alt="...">
-   
-    </div>
-    <div class="item">
-      <img class="img-responsive" src="images/ch1.jpg" alt="...">
-    </div>
+	</div>
 	
-	<div class="item">
-      <img class="img-responsive" src="images/ch2.jpg" alt="...">
-    </div>
-	<div class="item">
-      <img class="img-responsive" src="images/ch3.jpg" alt="...">
-    </div>
-	<div class="item">
-      <img  class="img-responsive" src="images/ch4.jpg" alt="...">
-    </div>
-	<div class="item">
-      <img class="img-responsive" src="images/ch5.jpg" alt="...">
-    </div>
-	<div class="item">
-      <img class="img-responsive" src="images/ch6.jpg" alt="...">
-    </div>
-	<div class="item">
-      <img class="img-responsive" src="images/ch7.jpg" alt="...">
-    </div>
-	<div class="item">
-      <img class="img-responsive" src="images/ch8.jpg" alt="...">
-    </div>
-	<div class="item">
-      <img class="img-responsive" src="images/ch9.jpg" alt="...">
-    </div>
-	<div class="item">
-      <img class="img-responsive" src="images/ch10.jpg" alt="...">
-    </div>
-	<div class="item">
-      <img class="img-responsive" src="images/ch11.jpg" alt="...">
-    </div><div class="item">
-      <img class="img-responsive" src="images/ch12.jpg" alt="...">
-    </div>
+	<div class="row">
 	
-	<div class="item">
-      <img class="img-responsive" src="images/ch14.jpg" alt="...">
-    </div>
-    
-  </div>
+		<div class="col-md-4">
+			      <img class="img-responsive" src="images/ch6.jpg" alt="...">
 
-  <!-- Controls -->
+			</div>
+			
+		<div class="col-md-4">
+			      <img class="img-responsive" src="images/ch7.jpg" alt="...">
 
+			</div>
+			
+		<div class="col-md-4">
+			      <img class="img-responsive" src="images/ch8.jpg" alt="...">
+
+			</div>
+	</div>
+	
+	
+	
+	<div class="row">
+	
+		<div class="col-md-4">
+			      <img class="img-responsive" src="images/ch9.jpg" alt="...">
+
+			</div>
+			
+		<div class="col-md-4">
+			      <img class="img-responsive" src="images/ch10.jpg" alt="...">
+
+			</div>
+			
+		<div class="col-md-4">
+			      <img class="img-responsive" src="images/ch11.jpg" alt="...">
+
+			</div>
+	</div>
+			    
   
-</div>
+  
+  <div class="row">
+	
+		<div class="col-md-4">
+			      <img class="img-responsive" src="images/ch12.jpg" alt="...">
+
+			</div>
+			
+		<div class="col-md-4">
+			      <img class="img-responsive" src="images/ch14.jpg" alt="...">
+
+			</div>
+			
+		
+	</div>
     
-      </div>
+     
     </div>
 		</div>
 </div>
