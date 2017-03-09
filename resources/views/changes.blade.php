dacff741ad0170beec9bb4bb4f49960894263793
Yehor Bobrov <yehorbobrov8@gmail.com>
2017-03-09 02:04:11 +0200

    fix do i posle

--- resources/views/changes.blade.php
+++ resources/views/changes.blade.php
@@ -3,7 +3,14 @@
 @section('content')
    <div class="container">
 	<div class="row">
-			
+	<div class="page-header">
+            <h2>Преображение людей</h2>
+        </div>
+		<div class='col-md-12' style="background: rgba(255,255,255,0.4); padding-top: 15px">
+		<p style="font-weight: bold;color: #000000;">Невероятное преображение всех этих людей связано с тем, что они сумели избавиться от наркотической зависимости.
+		Когда-то все они употребляли тяжелые наркотики,алкоголь чувствуя потребность убежать от жизни, но теперь они живут совершенно другой жизни. 
+		Об ужасах тех дней теперь им напоминают лишь эти  фото.</p>
+        </div>		
 		<div class="col-md-4">
 			      <img class="img-responsive" src="images/ch13.jpg" alt="...">
 
